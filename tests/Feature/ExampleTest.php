ff9a0d7c2535fe954b9c9fd79acee8355e83f1f0
jeremykenedy <jeremykenedy@gmail.com>
2018-09-30 23:46:10 -0700

    update test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -13,7 +13,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $response = $this->get('/projects');
 
         $response->assertStatus(200);
     }
